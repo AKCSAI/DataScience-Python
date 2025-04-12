986c41362cfe7a7d73c8a947ce9ed3bf757a22e1
Aziz Khan <manruption@gmail.com>
2025-04-12 14:48:35 -0400

    Code for selecting certain element in an array

--- Subset.py
+++ Subset.py
@@ -1,12 +1,15 @@
-# Create the areas list
-areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+# Create the areas list with alternating room names and their sizes in square meters
+areas = ["hallway", 11.25, 
+         "kitchen", 18.0, 
+         "living room", 20.0, 
+         "bedroom", 10.75, 
+         "bathroom", 9.50]
 
-# Print out second element from areas
+# Print out the second element from the list, which is the size of the hallway (index 1)
 print(areas[1])
 
-# Print out last element from areas
+# Print out the last element in the list, which is the size of the bathroom
 print(areas[-1])
 
-# Print out the area of the living room
+# Print out the area of the living room, which is at index 5 in the list
 print(areas[5])
-
